40e329da250233106e86f6e791dad6cd7cbbbfb1
Nicole Russo <nicolecrst@gmail.com>
2023-04-12 17:19:01 -0400

    fix: temporarily fixing index errors

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,7 +1,37 @@
-export const Modal = () => {
+import { useState } from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import { Box } from './style';
+
+export const ModalGeneral = () => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <>
-        
+            <Button onClick={handleOpen}>Open modal</Button>
+
+            <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+            backdrop: {
+                timeout: 500,
+            },
+            }}>
+                <Fade in={open}>
+                    <Box>
+                        <span>aaaaaa</span>
+                    </Box>
+                </Fade>
+            </Modal>
         </>
     )
 }
