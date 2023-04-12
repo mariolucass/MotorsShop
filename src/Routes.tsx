a4b98859574f38320c7dfde22e3233c3c06d3a20
Filipe Costa <84087378+FILIPECosta77@users.noreply.github.com>
2023-04-12 19:32:47 -0300

    Fix duplicate files

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/home/index";
 import AdvertPage from "./pages/especificAdvert";
 
 const PageRoutes = () => {
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,7 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { Home } from "../pages/Home"
+import React from "react";
 
-export const RoutesMain = () => {
-    return(
-        <Routes>
-            <Route path="/home" element={<Home></Home>}></Route>
-            <Route path="*" element={<Navigate to="/home"/>}></Route>
-        </Routes>
-    )
-}
+const index = () => {
+  return <div>index</div>;
+};
+
+export default index;
