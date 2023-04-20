2109de47a050c502ad567db37f43f06716d94ce9
Nicole Russo <nicolecrst@gmail.com>
2023-04-20 07:31:19 -0400

    refactor: removing useEffects and DOM from login component

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,25 +1,12 @@
-import { useEffect, useState } from "react";
 import { Header } from "../../components/header"
 import { Login } from "../../components/login";
 import { Footer } from "../../components/footer";
 
 export const LoginPage = () => {
-    const [widthSize, setWidthSize] = useState(window.innerWidth);
-
-    const setSize = () => {
-      setWidthSize(window.innerWidth);
-    };
-  
-    useEffect(() => {
-      window.addEventListener("resize", setSize);
-      return () => {
-        window.removeEventListener("resize", setSize);
-      };
-    }, [widthSize]);
 
     return (
         <>
-            <Header widthSize={widthSize}></Header>
+            <Header isHome={true}></Header>
             <Login></Login>
             <Footer></Footer>
         </>
