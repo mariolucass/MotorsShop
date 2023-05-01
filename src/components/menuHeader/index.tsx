c89ea72469d9c67748c207e8d14411a034c8f198
Lucas <lucassoares2007@hotmail.com>
2023-05-01 00:00:13 -0300

    refactor: modal update user

--- src/components/menuHeader/index.tsx
+++ src/components/menuHeader/index.tsx
@@ -4,55 +4,61 @@ import { useModalContext, useUserContext } from "../../context";
 import { useNavigate } from "react-router-dom";
 import * as framer from "../../libs/framer";
 import { ModalUpdateDeleteUser } from "../modal/modalUpdateDeleteUser";
-import * as framer from "../../libs/framer";
-import { useUserContext } from "../../context";
+
 interface IProps {
   isOpen: boolean;
 }
+
 export const MenuHeader = ({ isOpen }: IProps) => {
   const { userData, logoutUser } = useUserContext();
   const navigate = useNavigate();
-  const {handleOpen, handleOpenAddress} = useModalContext()
+  const { handleOpen, handleOpenAddress } = useModalContext();
 
   return (
     <>
-    <motion.nav className="menu">
-      <ListMenuStyled
-        animate={isOpen ? framer.animateShownUl : framer.animateHiddenUl}
-        style={{
-          pointerEvents: isOpen ? "auto" : "none",
-          clipPath: "inset(10% 50% 90% 50% round 10px)",
-        }}
-      >
-        <motion.li
-          animate={isOpen ? framer.animateShownLi : framer.animateHiddenLi} onClick={()=> {handleOpen()}}
+      <motion.nav className="menu">
+        <ListMenuStyled
+          animate={isOpen ? framer.animateShownUl : framer.animateHiddenUl}
+          style={{
+            pointerEvents: isOpen ? "auto" : "none",
+            clipPath: "inset(10% 50% 90% 50% round 10px)",
+          }}
         >
-          Editar Perfil
-        </motion.li>
-
-        <motion.li
-          animate={isOpen ? framer.animateShownLi : framer.animateHiddenLi}
-         onClick={() => {handleOpenAddress()}}>
-          Editar endereco
-        </motion.li>
-
-        {userData?.role === "SELLER" && (
           <motion.li
-            onClick={() => navigate("/profile")}
+            animate={isOpen ? framer.animateShownLi : framer.animateHiddenLi}
+            onClick={() => {
+              handleOpen();
+            }}
+          >
+            Editar Perfil
+          </motion.li>
+
+          <motion.li
+            animate={isOpen ? framer.animateShownLi : framer.animateHiddenLi}
+            onClick={() => {
+              handleOpenAddress();
+            }}
+          >
+            Editar endereco
+          </motion.li>
+
+          {userData?.role === "SELLER" && (
+            <motion.li
+              onClick={() => navigate("/profile")}
+              animate={isOpen ? framer.animateShownLi : framer.animateHiddenLi}
+            >
+              Meus Anúncios
+            </motion.li>
+          )}
+
+          <motion.li
+            onClick={logoutUser}
             animate={isOpen ? framer.animateShownLi : framer.animateHiddenLi}
           >
-            Meus Anúncios
+            Sair
           </motion.li>
-        )}
-
-        <motion.li
-          onClick={logoutUser}
-          animate={isOpen ? framer.animateShownLi : framer.animateHiddenLi}
-        >
-          Sair
-        </motion.li>
-      </ListMenuStyled>
-    </motion.nav>
+        </ListMenuStyled>
+      </motion.nav>
     </>
   );
 };
--- src/components/modal/modalUpdateDeleteUser/index.tsx
+++ src/components/modal/modalUpdateDeleteUser/index.tsx
@@ -1,23 +1,23 @@
-import { useForm } from "react-hook-form"
-import { ModalGeneral } from ".."
-import { Input } from "../../inputs"
-import { Container, Form, DivisionTypes, Buttons } from "./style"
-import { AiOutlineClose } from "react-icons/ai"
-import { useEffect } from "react"
-import { useUserContext } from "../../../context/UserContext"
-import Button from '@mui/material/Button';
-import { useModalContext } from "../../../context"
-import { deleteUser, patchUser } from "../../../services/apiUser"
+import { useForm } from "react-hook-form";
+import { ModalGeneral } from "..";
+import { Input } from "../../inputs";
+import { Container, Form, DivisionTypes, Buttons } from "./style";
+import { AiOutlineClose } from "react-icons/ai";
+import { useEffect } from "react";
+import { useUserContext } from "../../../context/UserContext";
+import Button from "@mui/material/Button";
+import { useModalContext } from "../../../context";
+import { deleteUser, patchUser } from "../../../services/apiUser";
 
 interface IUserUpdate {
-    name?: string
-    email?: string
-    password?: string
-    phone?: string
-    birthdate?: Date
-    description?: string 
-    role?: string 
-/*     zip_code?: string
+  name?: string;
+  email?: string;
+  password?: string;
+  phone?: string;
+  birthdate?: Date;
+  description?: string;
+  role?: string;
+  /*     zip_code?: string
     state?: string
     street?: string
     city?: string
@@ -26,72 +26,123 @@ interface IUserUpdate {
 }
 
 export const ModalUpdateDeleteUser = () => {
-    const {userData} = useUserContext()
-    const {register, handleSubmit, setValue} = useForm()
-    const {handleClose, handleOpen} = useModalContext()
+  const { userData } = useUserContext();
+  const { register, handleSubmit, setValue } = useForm();
+  const { handleClose, handleOpen } = useModalContext();
 
-    const onSubmit = async (data: IUserUpdate ) => {
-        console.log(data)
-        let response = await patchUser(data, userData!.id)
-        handleClose()
-    }
+  const onSubmit = async (data: IUserUpdate) => {
+    let response = await patchUser(data, userData!.id);
+    handleClose();
+  };
 
-    const deleteUserId = async () => {
-        await deleteUser(userData!.id)
-        document.location.reload();
-    }
+  const deleteUserId = async () => {
+    await deleteUser(userData!.id);
+    document.location.reload();
+  };
 
-    useEffect(() => {
-        setValue("name", userData ? userData.name : "")
-        setValue("email", userData ? userData.email : "")
-        setValue("phone", userData ? userData.phone : "")
-        setValue("cpf", userData ? userData.cpf : "")
-        setValue("birthdate", userData ? userData.birthdate : "")
-        setValue("description", userData ? userData.description : "")
-        setValue("role", userData ? userData.role : "")
-    }, [handleOpen])
+  useEffect(() => {
+    setValue("name", userData ? userData.name : "");
+    setValue("email", userData ? userData.email : "");
+    setValue("phone", userData ? userData.phone : "");
+    setValue("cpf", userData ? userData.cpf : "");
+    setValue("birthdate", userData ? userData.birthdate : "");
+    setValue("description", userData ? userData.description : "");
+    setValue("role", userData ? userData.role : "");
+  }, [handleOpen]);
 
-    return (
-        <ModalGeneral>
-            <Container>
-                <div id="title">
-                    <span>Editar perfil</span>
-                    <AiOutlineClose onClick={()=>{handleClose()}}/>
-                </div>
-                <h3 id="subtitle">Informações pessoais</h3>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <Input label="Nome" name="name" width="100%" placeholder="Nome completo" register={register}/>
-                    <Input label="Email" name="email" width="100%" placeholder="Email" register={register}/>
-                    {/* <Input label="Senha" name="password" width="100%" placeholder="Senha" register={register}/> */}
-                    <Input label="CPF" name="cpf" width="100%" placeholder="000.000.000-00" register={register}/>
-                    <Input label="Celular" name="phone" width="100%" placeholder="(00) 00000-0000" register={register}/>
-                    <Input label="Data de Nascimento" name="birthdate" width="100%" placeholder="DD/MM/AAAA" register={register}/>
-                    <textarea placeholder="Descrição..." {...register("description")}>
-                        dsadasdas
-                    </textarea>
-                    <span>Tipo de conta</span>
-                    <DivisionTypes>
-                        <label className="radio">
-                        <input
-                            type="radio"
-                            value={"BUYER"}
-                            {...register("role")}
-                            checked
-                        />
-                        <span>Comprador</span>
-                        </label>
-                        <label className="radio">
-                        <input type="radio" value={"SELLER"} {...register("role")} />
-                        <span>Anunciante</span>
-                        </label>
-                    </DivisionTypes> 
-                    <Buttons>
-                        <Button variant="contained" id="cancel" onClick={()=>{handleClose()}}>Cancelar</Button>
-                        <Button variant="contained" id="delete" onClick={() => {deleteUserId()}}>Excluir</Button>
-                        <Button variant="contained" id="update" type="submit">Salvar Alterações</Button>
-                    </Buttons>
-                </Form>
-            </Container>
-        </ModalGeneral>
-    )
-}
+  return (
+    <ModalGeneral>
+      <Container>
+        <div id="title">
+          <span>Editar perfil</span>
+          <AiOutlineClose
+            onClick={() => {
+              handleClose();
+            }}
+          />
+        </div>
+        <h3 id="subtitle">Informações pessoais</h3>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            label="Nome"
+            name="name"
+            width="100%"
+            placeholder="Nome completo"
+            register={register}
+          />
+          <Input
+            label="Email"
+            name="email"
+            width="100%"
+            placeholder="Email"
+            register={register}
+          />
+          {/* <Input label="Senha" name="password" width="100%" placeholder="Senha" register={register}/> */}
+          <Input
+            label="CPF"
+            name="cpf"
+            width="100%"
+            placeholder="000.000.000-00"
+            register={register}
+          />
+          <Input
+            label="Celular"
+            name="phone"
+            width="100%"
+            placeholder="(00) 00000-0000"
+            register={register}
+          />
+          <Input
+            label="Data de Nascimento"
+            name="birthdate"
+            width="100%"
+            placeholder="DD/MM/AAAA"
+            register={register}
+          />
+          <textarea placeholder="Descrição..." {...register("description")}>
+            dsadasdas
+          </textarea>
+          <span>Tipo de conta</span>
+          <DivisionTypes>
+            <label className="radio">
+              <input
+                type="radio"
+                value={"BUYER"}
+                {...register("role")}
+                checked
+              />
+              <span>Comprador</span>
+            </label>
+            <label className="radio">
+              <input type="radio" value={"SELLER"} {...register("role")} />
+              <span>Anunciante</span>
+            </label>
+          </DivisionTypes>
+          <Buttons>
+            <Button
+              variant="contained"
+              id="cancel"
+              onClick={() => {
+                handleClose();
+              }}
+            >
+              Cancelar
+            </Button>
+            <Button
+              variant="contained"
+              id="delete"
+              onClick={() => {
+                deleteUserId();
+              }}
+            >
+              Excluir
+            </Button>
+            <Button variant="contained" id="update" type="submit">
+              Salvar Alterações
+            </Button>
+          </Buttons>
+        </Form>
+      </Container>
+    </ModalGeneral>
+  );
+};
